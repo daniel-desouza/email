6e467333660f556d5056257c46fafb1a90c2d359
Daniel de Souza <daniel.desouza18@gmail.com>
2020-02-12 10:06:19 -0500

    add all demo emails to /send endpoint

--- emaildemo/src/main/java/com/example/emaildemo/controller/EmailController.java
+++ emaildemo/src/main/java/com/example/emaildemo/controller/EmailController.java
@@ -4,16 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import com.example.emaildemo.service.FreemarkerEmailService;
+import com.example.emaildemo.service.ThymeleafEmailService;
 import com.example.emaildemo.service.VelocityEmailService;
 
 @RestController
 public class EmailController {
-	
+
 	@Autowired
 	VelocityEmailService emailService;
-	
+
+	@Autowired
+	FreemarkerEmailService freemarkerEmailService;
+
+	@Autowired
+	ThymeleafEmailService thymeleafEmailService;
+
 	@GetMapping("/send")
 	public void sendEmail(String name) {
-		emailService.sendEmail("Test Email from Controller", "test@test.test", "daniel@daniel.daniel");
+		emailService.sendEmail("Test Velocity Email from Controller", "test@test.test", "daniel@daniel.daniel");
+		try {
+			freemarkerEmailService.sendSimpleMessage();
+		} catch (MessagingException | IOException | TemplateException e) {
+			e.printStackTrace();
+		}
+		thymeleafEmailService.sendMail("Test Thymeleaf Email from Controller", "test@test.test", "daniel@daniel.daniel");
 	}
 }
